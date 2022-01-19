e13f025942b5fc6138aa5f25ebefb53a4c35839c
Parmaklot <babar.dmitry@gmail.com>
2022-01-19 11:57:21 +0300

    feat: Implement themes
    Current theme is stored in local storage. If nothing is stored, pick
    default based on CSS media feature prefers-color-scheme.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <p>Initialize project using Create React App.</p>;
-}
+import useLocalStorage from 'use-local-storage';
+import { Theme } from 'types/theme';
+import styles from './App.module.scss';
+
+const App: React.FC = () => {
+  const darkIsDefault = window.matchMedia(
+    '(prefers-color-scheme: dark)'
+  ).matches;
+  const defaultTheme = darkIsDefault ? 'dark' : 'light';
+
+  const [theme, setTheme] = useLocalStorage<Theme>('theme', defaultTheme);
+
+  return <div className={styles[`Root_theme_${theme}`]}></div>;
+};
 
 export default App;
