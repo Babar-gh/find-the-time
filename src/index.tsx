f09bf66a143c0c02db7d361886671e778295f8f5
Parmaklot <babar.dmitry@gmail.com>
2022-01-19 09:58:22 +0300

    feat: Add index.css

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from 'store/store';
 import './initAxios';
+import './index.css';
 import App from 'App';
 import reportWebVitals from 'reportWebVitals';
 
