f367f51c136c79e549db503bfef6bd029eb3c973
Parmaklot <babar.dmitry@gmail.com>
2022-01-28 15:51:40 +0300

    feat: Add mock components for development only

---
+++ src/components/ui-kit/DummyList/DummyList.tsx
@@ -0,0 +1,20 @@
+const DummyList: React.FC = () => {
+  return (
+    <ul
+      style={{
+        color: 'var(--text-primary)',
+        listStyle: 'none',
+        padding: 0,
+        margin: 0,
+      }}
+    >
+      <li>Lorem</li>
+      <li>Ipsum</li>
+      <li>Dolor</li>
+      <li>Sit</li>
+      <li>Amet</li>
+    </ul>
+  );
+};
+
+export default DummyList;
