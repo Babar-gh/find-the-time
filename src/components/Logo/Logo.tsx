b9d9e445785fea4bbc22997e37318771b45b4ad0
Parmaklot <babar.dmitry@gmail.com>
2022-01-28 15:49:23 +0300

    refactor: Make Logo a stand-alone component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,13 +1,12 @@
-import { Link } from 'react-router-dom';
 import { ReactComponent as LogoIcon } from 'assets/icons/event_available.svg';
 import styles from './Logo.module.scss';
 
 const Logo: React.FC = () => {
   return (
-    <Link to="/" className={styles['Root']}>
+    <div className={styles['Root']}>
       <LogoIcon className={styles['Icon']} />
       <span className={styles['Text']}>Find the Time</span>
-    </Link>
+    </div>
   );
 };
 
