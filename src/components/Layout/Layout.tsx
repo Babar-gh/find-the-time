6915219f400b03aefc364ab045549736cf2ba8bb
Parmaklot <babar.dmitry@gmail.com>
2022-02-01 10:51:06 +0300

    feat: Change the mobile version of the Layout
    Also change some names and make the order of imports to comply with
    ESLint rules.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,39 +1,51 @@
 import { useState } from 'react';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 import { ReactComponent as MenuIcon } from 'assets/icons/menu.svg';
 import DummyContent from 'components/ui-kit/DummyContent';
+import DummyMenu from 'components/ui-kit/DummyList';
 import Logo from '../Logo';
 import styles from './Layout.module.scss';
-import DummyMenu from 'components/ui-kit/DummyList';
-import { Link } from 'react-router-dom';
 
 const cn = classNames.bind(styles);
 
 const Layout: React.FC = () => {
-  const [menuIsHiddenOnSmallScreens, setMenuIsHiddenOnSmallScreens] =
+  const [sidebarIsHiddenOnSmallScreens, setSidebarIsHiddenOnSmallScreens] =
     useState(true);
+
+  const topBarLeftPart = (
+    <>
+      <button
+        className={styles['MenuButton']}
+        onClick={() =>
+          setSidebarIsHiddenOnSmallScreens((isHidden) => !isHidden)
+        }
+      >
+        <MenuIcon className={styles['MenuIcon']} />
+      </button>
+      <Link to="/" className={styles['LogoContainer']}>
+        <Logo />
+      </Link>
+    </>
+  );
 
   return (
     <div className={styles['Root']}>
-      <header className={styles['Header']}>
-        <button
-          className={styles['MenuButton']}
-          onClick={() => setMenuIsHiddenOnSmallScreens((isHidden) => !isHidden)}
-        >
-          <MenuIcon className={styles['MenuIcon']} />
-        </button>
-        <Link to="/" className={styles['LogoContainer']}>
-          <Logo />
-        </Link>
-      </header>
-      <div className={styles['Wrapper']}>
+      <header className={styles['Header']}>{topBarLeftPart}</header>
+      <div className={styles['ColumnWrapper']}>
         <nav
-          className={cn('MenuContainer', {
-            MenuContainer_hiddenOnSmallScreens: menuIsHiddenOnSmallScreens,
+          className={cn('Sidebar', {
+            Sidebar_hiddenOnSmallScreens: sidebarIsHiddenOnSmallScreens,
           })}
         >
+          <header className={styles['SidebarHeader']}>{topBarLeftPart}</header>
           <DummyMenu />
         </nav>
+
+        {!sidebarIsHiddenOnSmallScreens && (
+          <div className={styles['SidebarScrim']} />
+        )}
+
         <main className={styles['ContentContainer']}>
           <DummyContent />
         </main>
