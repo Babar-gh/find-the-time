ac2392b1ac0d8ec77aae641f8202ddf8e683952c
Parmaklot <babar.dmitry@gmail.com>
2022-02-02 18:09:56 +0300

    feat: Conditionaly render Layout's elements
    Insted of using `display: none` CSS rule conditionally render some parts
    of the Layout based on the current breakpoint

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,69 +1,95 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import classNames from 'classnames/bind';
 
-import { ReactComponent as MenuIcon } from 'assets/icons/menu.svg';
+import Modal from 'components/ui-kit/Modal';
 import Logo from 'components/Logo';
 import DummyContent from 'components/ui-kit/DummyContent';
 import DummyMenu from 'components/ui-kit/DummyList';
+import { ReactComponent as MenuIcon } from 'assets/icons/menu.svg';
 
 import styles from './Layout.module.scss';
-
-const cn = classNames.bind(styles);
 
 interface IProps {
   onThemeSwitch: () => void;
 }
 
 const Layout: React.FC<IProps> = ({ onThemeSwitch }) => {
-  const [sidebarIsHiddenOnSmallScreens, setSidebarIsHiddenOnSmallScreens] =
-    useState(true);
+  const [sidebarIsCollapsed, setSidebarIsCollapsed] = useState(true);
 
-  const topBarLeftPart = (
-    <>
-      <button
-        className={styles['MenuButton']}
-        onClick={() =>
-          setSidebarIsHiddenOnSmallScreens((isHidden) => !isHidden)
-        }
-      >
-        <MenuIcon className={styles['MenuIcon']} />
-      </button>
-      <Link to="/" className={styles['LogoContainer']}>
-        <Logo />
-      </Link>
-    </>
+  /* ——————————————— To be replaced with proper functionality ——————————————— */
+
+  type Breakpoint = 'Mobile' | 'Tablet' | 'Laptop' | 'Desktop';
+  const [breakpoint, setBreakpoint] = useState<Breakpoint>('Desktop');
+
+  const bp = (...breakpoints: Breakpoint[]) => {
+    return breakpoints.some((bp) => bp === breakpoint);
+  };
+
+  const temporaryBreakpointSelect = (
+    <select
+      name="choice"
+      value={breakpoint}
+      onChange={(event) => setBreakpoint(event.target.value as Breakpoint)}
+    >
+      <option value="Mobile">Mobile</option>
+      <option value="Tablet">Tablet</option>
+      <option value="Laptop">Laptop</option>
+      <option value="Desktop">Desktop</option>
+    </select>
+  );
+
+  const temporarySwitchThemeButton = (
+    <button onClick={onThemeSwitch}>Switch theme</button>
+  );
+
+  /* ———————————————————————————————————————————————————————————————————————— */
+
+  const menuButton = (
+    <button
+      className={styles['MenuButton']}
+      onClick={() => setSidebarIsCollapsed((isCollapsed) => !isCollapsed)}
+    >
+      <MenuIcon className={styles['MenuIcon']} />
+    </button>
+  );
+
+  const logo = (
+    <Link to="/" className={styles['LogoContainer']}>
+      <Logo />
+    </Link>
+  );
+
+  const sidebar = (
+    <nav className={styles['Sidebar']}>
+      {bp('Mobile') && (
+        <header className={styles['SidebarHeader']}>
+          {menuButton}
+          {logo}
+        </header>
+      )}
+      <DummyMenu />
+    </nav>
   );
 
   return (
     <div className={styles['Root']}>
       <header className={styles['Header']}>
-        {topBarLeftPart}
-        <button
-          style={{ marginLeft: 'auto', width: '60px' }}
-          onClick={onThemeSwitch}
-        >
-          Switch theme
-        </button>
+        {bp('Mobile', 'Tablet') && menuButton}
+        {logo}
       </header>
-      <div className={styles['ColumnWrapper']}>
-        <nav
-          className={cn('Sidebar', {
-            Sidebar_hiddenOnSmallScreens: sidebarIsHiddenOnSmallScreens,
-          })}
-        >
-          <header className={styles['SidebarHeader']}>{topBarLeftPart}</header>
-          <DummyMenu />
-        </nav>
 
-        {!sidebarIsHiddenOnSmallScreens && (
-          <div
-            className={styles['SidebarBackdrop']}
-            onClick={() => setSidebarIsHiddenOnSmallScreens(true)}
-          />
+      <div className={styles['ColumnWrapper']}>
+        {bp('Mobile') && !sidebarIsCollapsed && (
+          <Modal onBackdropClick={() => setSidebarIsCollapsed(true)}>
+            {sidebar}
+          </Modal>
         )}
+        {bp('Tablet') && !sidebarIsCollapsed && sidebar}
+        {bp('Laptop', 'Desktop') && sidebar}
 
         <main className={styles['Content']}>
+          {temporarySwitchThemeButton}
+          {temporaryBreakpointSelect}
           <DummyContent />
         </main>
       </div>
