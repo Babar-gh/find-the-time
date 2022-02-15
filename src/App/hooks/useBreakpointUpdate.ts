5905ed03f6feb0227d78a53c6e1ddbc8b0fff4fd
Parmaklot <babar.dmitry@gmail.com>
2022-02-15 09:21:59 +0300

    feat: Clean up listeners in useBreakpointUpdate

--- src/App/hooks/useBreakpointUpdate.ts
+++ src/App/hooks/useBreakpointUpdate.ts
@@ -1,31 +1,38 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Breakpoint, BREAKPOINT_QUERIES } from 'constants/breakpoints';
 import { useAppDispatch } from 'store/hooks';
 import { setActive } from 'store/slices/breakpoints';
 
+type ActiveBreakpointDispatch = (
+  query: MediaQueryList | MediaQueryListEvent
+) => void;
+
 const useBreakpointUpdate = () => {
   const dispatch = useAppDispatch();
 
-  const dispatchActiveBreakpoint = useCallback(
-    (query: MediaQueryList | MediaQueryListEvent, breakpoint: Breakpoint) => {
-      if (query.matches) {
-        dispatch(setActive(breakpoint));
-      }
-    },
-    [dispatch]
-  );
-
   useEffect(() => {
+    const listenersToCleanUp: [MediaQueryList, ActiveBreakpointDispatch][] = [];
+
     Object.entries(BREAKPOINT_QUERIES).forEach(([breakpoint, queryString]) => {
       const query = window.matchMedia(queryString);
 
-      dispatchActiveBreakpoint(query, breakpoint as Breakpoint);
+      const dispatchActiveBreakpoint: ActiveBreakpointDispatch = (query) => {
+        if (query.matches) {
+          dispatch(setActive(breakpoint as Breakpoint));
+        }
+      };
 
-      query.addEventListener('change', (event) =>
-        dispatchActiveBreakpoint(event, breakpoint as Breakpoint)
-      );
+      dispatchActiveBreakpoint(query);
+      query.addEventListener('change', dispatchActiveBreakpoint);
+
+      listenersToCleanUp.push([query, dispatchActiveBreakpoint]);
     });
-  }, [dispatchActiveBreakpoint]);
+
+    return () =>
+      listenersToCleanUp.forEach(([query, handler]) =>
+        query.removeEventListener('change', handler)
+      );
+  }, [dispatch]);
 };
 
 export default useBreakpointUpdate;
