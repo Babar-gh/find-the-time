bdc2450f65dba88cf3d31344e8e2802766c382d3
Parmaklot <babar.dmitry@gmail.com>
2022-02-16 12:14:52 +0300

    feat(Menu): Render children as <li> elements
    Also pass down isActive prop based on comparison between activeId prop
    and child's id.

--- src/ui-kit/Menu/Menu.tsx
+++ src/ui-kit/Menu/Menu.tsx
@@ -1,11 +1,28 @@
-import MenuItem from './MenuItem';
+import { Children, cloneElement, isValidElement, ReactElement } from 'react';
+import Item from './components/Item';
+import MenuItem from './components/Item';
 
-//import styles from './Menu.module.scss';
+//import styles from './components/Menu.module.scss';
 
-type MenuComponent = React.FC & { Item: typeof MenuItem };
+interface IProps {
+  activeId: string;
+  children: ReactElement<typeof Item> | ReactElement<typeof Item>[];
+}
 
-const Menu: MenuComponent = ({ children }) => {
-  return <ul>{children}</ul>;
+type MenuComponent = React.FC<IProps> & { Item: typeof MenuItem };
+
+const Menu: MenuComponent = ({ activeId, children }) => {
+  return (
+    <ul>
+      {Children.map(children, (item) => {
+        if (isValidElement(item)) {
+          const isActive = item.props.id === activeId;
+
+          return <li>{cloneElement(item, { isActive })}</li>;
+        }
+      })}
+    </ul>
+  );
 };
 
 Menu.Item = MenuItem;
