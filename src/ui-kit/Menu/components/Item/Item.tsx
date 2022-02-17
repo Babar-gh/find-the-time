63baf7be61a3f061abadee333af0396ed0e5e764
Parmaklot <babar.dmitry@gmail.com>
2022-02-17 17:08:11 +0300

    refactor(Item): Add comment

--- src/ui-kit/Menu/components/Item/Item.tsx
+++ src/ui-kit/Menu/components/Item/Item.tsx
@@ -28,6 +28,31 @@ const cn = classNames.bind(styles);
 const Item: React.FC<IProps> = (props) => {
   const { type, icon, isSelected, children } = props;
 
+  // { type, icon, isSelected, children, ...rest } syntax would be preferable,
+  // however as of version 4.5.5, TypeScript can't narrow down the type of
+  // “rest” based on “type” value.
+
+  // Consider:
+
+  // const { type, icon, isSelected, children, ...rest } = props;
+  //
+  // if (type === 'Anchor') {
+  //   <LinkWrapper type={type} to={rest.to} />;
+  // }
+
+  // Error:
+  // Property 'to' does not exist on type '{ id: string; to: string; } | { id: string; onClick: MouseEventHandler<Element>; }'.
+  // Property 'to' does not exist on type '{ id: string; onClick: MouseEventHandler<Element>; }'.ts(2339)
+
+  // const { type, icon, isSelected, children } = props;
+  //
+  // if (type === 'Anchor') {
+  //   <LinkWrapper type={type} to={props.to} />;
+  // }
+
+  // Behaves as expected:
+  // props.to type is correctly narrowed to “(property) ILinkProps.to: string”
+
   const content = (
     <div className={cn('Container', { Container_selected: isSelected })}>
       <span className={styles['IconContainer']}>{icon}</span>
