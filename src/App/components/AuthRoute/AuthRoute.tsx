1fdeb20506c76f680643143bdef4448d3f6e79b4
Parmaklot <babar.dmitry@gmail.com>
2022-03-04 15:02:18 +0300

    refactor(AuthRoute, PrivateRoute): Simplify syntax

--- src/App/components/AuthRoute/AuthRoute.tsx
+++ src/App/components/AuthRoute/AuthRoute.tsx
@@ -2,7 +2,11 @@ import { Navigate, Route, RouteProps } from 'react-router-dom';
 import * as jwt from 'jwt';
 
 const AuthRoute: React.VFC<RouteProps> = (props) => {
-  return jwt.checkIfExists() ? <Navigate to={'/'} /> : <Route {...props} />;
+  if (jwt.checkIfExists()) {
+    return <Navigate to="/" />;
+  }
+
+  return <Route {...props} />;
 };
 
 export default AuthRoute;
--- src/App/components/PrivateRoute/PrivateRoute.tsx
+++ src/App/components/PrivateRoute/PrivateRoute.tsx
@@ -2,11 +2,11 @@ import { Navigate, Route, RouteProps } from 'react-router-dom';
 import * as jwt from 'jwt';
 
 const PrivateRoute: React.VFC<RouteProps> = (props) => {
-  return jwt.checkIfExists() ? (
-    <Route {...props} />
-  ) : (
-    <Navigate to={'/login'} />
-  );
+  if (jwt.checkIfExists()) {
+    return <Route {...props} />;
+  }
+
+  return <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
