459026e932e1db0f5a259f3d71537bf9a24e0dd7
Parmaklot <babar.dmitry@gmail.com>
2022-03-25 12:11:08 +0300

    feat(assets): Add a Logout icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,5 @@
 import { ReactComponent as EventAvailable } from './EventAvailable.svg';
 import { ReactComponent as Menu } from './Menu.svg';
+import { ReactComponent as Logout } from './Logout.svg';
 
-export default { EventAvailable, Menu };
+export default { EventAvailable, Menu, Logout };
