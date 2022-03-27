35a7b98d224c400530fe46b7d5a6e8af62d1e5c0
Parmaklot <babar.dmitry@gmail.com>
2022-03-27 13:36:24 +0300

    feat(assets): Add AccountCircle and Settings icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,7 @@
+import { ReactComponent as AccountCircle } from './AccountCircle.svg';
 import { ReactComponent as EventAvailable } from './EventAvailable.svg';
-import { ReactComponent as Menu } from './Menu.svg';
 import { ReactComponent as Logout } from './Logout.svg';
+import { ReactComponent as Menu } from './Menu.svg';
+import { ReactComponent as Settings } from './Settings.svg';
 
-export default { EventAvailable, Menu, Logout };
+export default { AccountCircle, EventAvailable, Logout, Menu, Settings };
