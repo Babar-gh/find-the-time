526b04e238d833b05ada521a69770ce738b329e0
Parmaklot <babar.dmitry@gmail.com>
2022-03-28 12:19:02 +0300

    feat(App): Add a dev-only Loader demonstration

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,15 @@
-import { useEffect } from 'react';
-import DummyContent from 'ui-kit/DummyContent';
+import { useEffect, useState } from 'react';
+import Button from 'ui-kit/Button';
 import Layout from 'components/Layout';
+import Loader from 'ui-kit/Loader';
+import Login from 'components/Login';
 import bodyStyles from './Body.module.scss';
 import useBreakpointUpdate from './hooks/useBreakpointUpdate';
 import useTheme from './hooks/useTheme';
 
 const App: React.VFC = () => {
   const [theme, switchTheme] = useTheme();
+  const [loaderIsShown, setLoaderIsShown] = useState(false);
 
   useEffect(() => {
     document.body.className = bodyStyles[`Root_theme_${theme}`];
@@ -17,7 +20,16 @@ const App: React.VFC = () => {
   return (
     <div>
       <Layout onThemeSwitch={switchTheme}>
-        <DummyContent />
+        <Button
+          elementProps={{
+            onClick: () => setLoaderIsShown((current) => !current),
+          }}
+        >
+          Show loader
+        </Button>
+        <Loader isShown={loaderIsShown}>
+          <Login />
+        </Loader>
       </Layout>
     </div>
   );
