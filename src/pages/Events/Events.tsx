462019c965c8db26fa4a3689c62af0c478bb4f7d
Parmaklot <babar.dmitry@gmail.com>
2022-03-31 15:04:09 +0300

    feat(Events): Map the list of EventTile components

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -1,7 +1,18 @@
 import Page from 'ui-kit/Page';
+import EventTile from './components/EventTile';
+import { DUMMY_RESPONSE_DATA } from './dummyResponseData';
+import styles from './Events.module.scss';
 
 const Events: React.VFC = () => {
-  return <Page title="Events"></Page>;
+  return (
+    <Page title="Events">
+      <div className={styles['EventList']}>
+        {DUMMY_RESPONSE_DATA.items.map(({ id, ...rest }) => {
+          return <EventTile key={id} id={id} {...rest} />;
+        })}
+      </div>
+    </Page>
+  );
 };
 
 export default Events;
