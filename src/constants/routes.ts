7e9177217487c766a1f7650deae9ce058cd8277b
Parmaklot <babar.dmitry@gmail.com>
2022-03-31 13:15:53 +0300

    feat(constants): Add enums for URL routes

---
+++ src/constants/routes.ts
@@ -0,0 +1,11 @@
+export enum AUTH {
+  Login = '/login',
+  Registration = '/registration',
+  ResetPassword = '/reset-password',
+}
+
+export enum PRIVATE {
+  Events = '/events',
+  Account = '/account',
+  Settings = '/settings',
+}
