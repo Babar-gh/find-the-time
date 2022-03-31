1a477dbd885da4e3b2f822491fed568ea5c03cbb
Parmaklot <babar.dmitry@gmail.com>
2022-03-31 13:11:15 +0300

    feat: Add a dev-only DummyPage

---
+++ src/pages/DummyPage/DummyPage.tsx
@@ -0,0 +1,10 @@
+import DummyContent from 'ui-kit/DummyContent';
+import Page from 'ui-kit/Page';
+
+const DummyPage: React.FC = () => (
+  <Page title="I’m an Example Page">
+    <DummyContent />
+  </Page>
+);
+
+export default DummyPage;
