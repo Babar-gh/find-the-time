bced01d72fc7750ce2c8e19c5bd6483f47deb800
Parmaklot <babar.dmitry@gmail.com>
2022-03-31 13:09:40 +0300

    feat: Add a Loading page

---
+++ src/pages/Loading/Loading.tsx
@@ -0,0 +1,10 @@
+import Loader from 'ui-kit/Loader';
+import Page from 'ui-kit/Page';
+
+const Loading: React.FC = () => (
+  <Loader isShown>
+    <Page title="" />
+  </Loader>
+);
+
+export default Loading;
