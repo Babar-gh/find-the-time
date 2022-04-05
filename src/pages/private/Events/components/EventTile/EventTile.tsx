deb34279dad474582a1187d1b1e8bbaddca66d8f
Parmaklot <babar.dmitry@gmail.com>
2022-04-05 16:16:09 +0300

    feat(EventTile, LoaderTile): Use the TileTemplate

--- src/pages/private/Events/components/EventTile/EventTile.tsx
+++ src/pages/private/Events/components/EventTile/EventTile.tsx
@@ -1,6 +1,7 @@
 import { forwardRef } from 'react';
 import Text from 'components/Text';
 import { IEvent } from 'api/types/events';
+import TileTemplate from '../TileTemplate';
 import styles from './EventTile.module.scss';
 
 interface IProps extends Omit<IEvent, 'subscriptions'> {}
@@ -8,25 +9,27 @@ interface IProps extends Omit<IEvent, 'subscriptions'> {}
 const EventTile = forwardRef<HTMLElement, IProps>(
   ({ title, created, location, comment, duration, chosenInterval }, ref) => {
     return (
-      <article className={styles['Root']} ref={ref}>
-        <h3>
-          <Text>{title}</Text>
-        </h3>
-        <p>
-          <Text>{`${created}, ${location}`}</Text>
-        </p>
-        <p>
-          <Text>{comment}</Text>
-        </p>
-        <p>
-          <Text>{duration.toString()}</Text>
-        </p>
-        {chosenInterval && (
+      <TileTemplate>
+        <article className={styles['Root']} ref={ref}>
+          <h3>
+            <Text>{title}</Text>
+          </h3>
           <p>
-            <Text>{`From ${chosenInterval.start} to ${chosenInterval.end} `}</Text>
+            <Text>{`${created}, ${location}`}</Text>
           </p>
-        )}
-      </article>
+          <p>
+            <Text>{comment}</Text>
+          </p>
+          <p>
+            <Text>{duration.toString()}</Text>
+          </p>
+          {chosenInterval && (
+            <p>
+              <Text>{`From ${chosenInterval.start} to ${chosenInterval.end} `}</Text>
+            </p>
+          )}
+        </article>
+      </TileTemplate>
     );
   }
 );
--- src/pages/private/Events/components/LoaderTile/LoaderTile.tsx
+++ src/pages/private/Events/components/LoaderTile/LoaderTile.tsx
@@ -1,15 +1,14 @@
 import { ComponentProps } from 'react';
 import Loader from 'ui-kit/Loader';
-import styles from './LoaderTile.module.scss';
+import TileTemplate from '../TileTemplate';
 
 interface IProps extends ComponentProps<typeof Loader> {}
 
-const LoaderTile: React.VFC<IProps> = (props) => {
-  return (
-    <Loader {...props}>
-      <div className={styles['Root']} />
+const LoaderTile: React.VFC<IProps> = ({ isShown }) =>
+  isShown ? (
+    <Loader isShown={true}>
+      <TileTemplate />
     </Loader>
-  );
-};
+  ) : null;
 
 export default LoaderTile;
