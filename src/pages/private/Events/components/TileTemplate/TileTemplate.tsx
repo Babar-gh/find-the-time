2b4493c8a5efb669c87d3ba92792b8bb370a69e0
Parmaklot <babar.dmitry@gmail.com>
2022-04-07 14:32:51 +0300

    feat(Events): Remove now unneeded subcomponents

--- src/pages/private/Events/components/TileTemplate/TileTemplate.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './TileTemplate.module.scss';
-
-const TileTemplate: React.FC = ({ children }) => (
-  <div className={styles['Root']}>{children}</div>
-);
-
-export default TileTemplate;
