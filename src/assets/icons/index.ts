5c3dcffb96444ea50b640fd6c68df36d03b66947
Parmaklot <babar.dmitry@gmail.com>
2022-04-07 14:23:07 +0300

    feat(assets): Add multiple new icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,25 @@
 import { ReactComponent as AccountCircle } from './AccountCircle.svg';
+import { ReactComponent as Description } from './Description.svg';
+import { ReactComponent as EditCalendar } from './EditCalendar.svg';
+import { ReactComponent as Event } from './Event.svg';
 import { ReactComponent as EventAvailable } from './EventAvailable.svg';
+import { ReactComponent as LocationOn } from './LocationOn.svg';
 import { ReactComponent as Logout } from './Logout.svg';
 import { ReactComponent as Menu } from './Menu.svg';
+import { ReactComponent as Person } from './Person.svg';
 import { ReactComponent as Settings } from './Settings.svg';
+import { ReactComponent as Timelapse } from './Timelapse.svg';
 
-export default { AccountCircle, EventAvailable, Logout, Menu, Settings };
+export default {
+  AccountCircle,
+  Description,
+  EditCalendar,
+  Event,
+  EventAvailable,
+  LocationOn,
+  Logout,
+  Menu,
+  Person,
+  Settings,
+  Timelapse,
+};
