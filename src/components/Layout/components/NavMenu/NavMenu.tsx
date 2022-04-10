59b94442ad906b4b3aa46b1b7f57f2d638331634
Parmaklot <babar.dmitry@gmail.com>
2022-04-10 15:52:42 +0300

    refactor(NavMenu): Add getMenuItem function

--- src/components/Layout/components/NavMenu/NavMenu.tsx
+++ src/components/Layout/components/NavMenu/NavMenu.tsx
@@ -1,28 +1,31 @@
+import { ComponentProps } from 'react';
 import { useLocation } from 'react-router-dom';
 import Menu from 'ui-kit/Menu';
 import { PRIVATE } from 'constants/routes';
 import styles from './NavMenu.module.scss';
+
+const getMenuItem = (
+  to: PRIVATE,
+  icon: ComponentProps<typeof Menu.Item>['icon'],
+  text: string
+) => (
+  <Menu.Item
+    id={to.slice(1)}
+    icon={icon}
+    elementProps={{ type: 'RouterLink', to }}
+  >
+    {text}
+  </Menu.Item>
+);
 
 const NavMenu: React.VFC = () => {
   const { pathname } = useLocation();
 
   return (
     <nav className={styles['Root']}>
-      <Menu selectedId={pathname}>
-        <Menu.Item
-          id={PRIVATE.Events}
-          icon="Event"
-          elementProps={{ type: 'RouterLink', to: PRIVATE.Events }}
-        >
-          Events
-        </Menu.Item>
-        <Menu.Item
-          id={PRIVATE.About}
-          icon="Info"
-          elementProps={{ type: 'RouterLink', to: PRIVATE.About }}
-        >
-          About
-        </Menu.Item>
+      <Menu selectedId={pathname.split('/')[1]}>
+        {getMenuItem(PRIVATE.Events, 'Event', 'Events')}
+        {getMenuItem(PRIVATE.About, 'Info', 'About')}
       </Menu>
     </nav>
   );
