6819133c8ef8f723f6e264e3c1095c31561f9271
Parmaklot <babar.dmitry@gmail.com>
2022-04-11 09:14:20 +0300

    feat(assets): Add multiple new icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,27 +1,45 @@
 import { ReactComponent as AccountCircle } from './AccountCircle.svg';
+import { ReactComponent as ArrowDownward } from './ArrowDownward.svg';
+import { ReactComponent as ArrowUpward } from './ArrowUpward.svg';
+import { ReactComponent as DarkMode } from './DarkMode.svg';
 import { ReactComponent as Description } from './Description.svg';
 import { ReactComponent as EditCalendar } from './EditCalendar.svg';
+import { ReactComponent as Email } from './Email.svg';
 import { ReactComponent as Event } from './Event.svg';
 import { ReactComponent as EventAvailable } from './EventAvailable.svg';
+import { ReactComponent as FilterList } from './FilterList.svg';
 import { ReactComponent as Info } from './Info.svg';
+import { ReactComponent as LightMode } from './LightMode.svg';
 import { ReactComponent as LocationOn } from './LocationOn.svg';
 import { ReactComponent as Logout } from './Logout.svg';
 import { ReactComponent as Menu } from './Menu.svg';
+import { ReactComponent as Password } from './Password.svg';
 import { ReactComponent as Person } from './Person.svg';
+import { ReactComponent as Search } from './Search.svg';
 import { ReactComponent as Settings } from './Settings.svg';
+import { ReactComponent as SortByAlpha } from './SortByAlpha.svg';
 import { ReactComponent as Timelapse } from './Timelapse.svg';
 
 export default {
   AccountCircle,
+  ArrowDownward,
+  ArrowUpward,
+  DarkMode,
   Description,
   EditCalendar,
+  Email,
   Event,
   EventAvailable,
+  FilterList,
   Info,
+  LightMode,
   LocationOn,
   Logout,
   Menu,
+  Password,
   Person,
+  Search,
   Settings,
+  SortByAlpha,
   Timelapse,
 };
