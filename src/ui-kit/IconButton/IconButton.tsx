600ebd4f93934f4998926c78b532eac7b3eb5dc9
Parmaklot <babar.dmitry@gmail.com>
2022-04-11 14:50:04 +0300

    feat(IconButton): Add 'isPressed' prop

--- src/ui-kit/IconButton/IconButton.tsx
+++ src/ui-kit/IconButton/IconButton.tsx
@@ -1,14 +1,18 @@
+import classNames from 'classnames/bind';
 import { ComponentProps, HTMLAttributes } from 'react';
 import Icon from 'components/Icon';
 import styles from './IconButton.module.scss';
 
 interface IProps extends Omit<HTMLAttributes<HTMLButtonElement>, 'className'> {
   icon: ComponentProps<typeof Icon>['type'];
+  isPressed?: boolean;
 }
 
-const IconButton: React.VFC<IProps> = ({ icon, ...rest }) => {
+const cn = classNames.bind(styles);
+
+const IconButton: React.VFC<IProps> = ({ icon, isPressed, ...rest }) => {
   return (
-    <button className={styles['Root']} {...rest}>
+    <button className={cn('Root', { Root_pressed: isPressed })} {...rest}>
       <Icon type={icon}></Icon>
     </button>
   );
