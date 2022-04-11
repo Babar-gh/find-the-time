d6a73826ce27f3d2a9ba2856f8da87376edfeb7a
Parmaklot <babar.dmitry@gmail.com>
2022-04-11 16:52:17 +0300

    feat(Events): Add a SearchInput subcomponent

---
+++ src/pages/private/Events/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,29 @@
+import { debounce } from 'lodash';
+import { useMemo } from 'react';
+import Input from 'ui-kit/Input';
+import useEventList from '../../hooks/useEventList';
+import styles from './SearchInput.module.scss';
+
+interface IProps extends Pick<ReturnType<typeof useEventList>, 'filter'> {
+  onFilterChange: ReturnType<typeof useEventList>['applyFilter'];
+}
+
+const SearchInput: React.VFC<IProps> = ({ filter, onFilterChange }) => {
+  const handleSearchChange = useMemo(
+    () =>
+      debounce((e: React.ChangeEvent<HTMLInputElement>): void => {
+        if (e.target.value !== filter.title) {
+          onFilterChange({ title: e.target.value });
+        }
+      }, 600),
+    [filter.title, onFilterChange]
+  );
+
+  return (
+    <div className={styles['Root']}>
+      <Input theme="alternative" icon="Search" onChange={handleSearchChange} />
+    </div>
+  );
+};
+
+export default SearchInput;
