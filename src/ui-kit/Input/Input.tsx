4a4843609fd894778f7a35f6a5cf6010ba565cd3
Parmaklot <babar.dmitry@gmail.com>
2022-04-11 09:18:22 +0300

    feat(Input): Add alternative theme, optional icon

--- src/ui-kit/Input/Input.tsx
+++ src/ui-kit/Input/Input.tsx
@@ -1,20 +1,36 @@
 import classNames from 'classnames/bind';
-import { InputHTMLAttributes } from 'react';
+import { ComponentProps, InputHTMLAttributes } from 'react';
+import Icon from 'components/Icon';
 import styles from './Input.module.scss';
 
 interface IProps
   extends Omit<InputHTMLAttributes<HTMLInputElement>, 'className'> {
-  validationStatus?: 'error' | 'warning'; // TODO: Add CSS for 'warning' theme.
+  icon?: ComponentProps<typeof Icon>['type'];
+  theme?: 'default' | 'alternative';
+  validationStatus?: 'error' | 'warning'; // TODO: Add CSS for 'warning' status.
 }
 
 const cn = classNames.bind(styles);
 
-const Input: React.VFC<IProps> = ({ validationStatus, ...rest }) => {
-  const className = cn('Root', {
-    [`Root_validation_${validationStatus}`]: validationStatus,
+const Input: React.VFC<IProps> = ({
+  icon,
+  theme = 'default',
+  validationStatus,
+  ...rest
+}) => {
+  const inputClassName = cn('Input', `Input_theme_${theme}`, {
+    [`Input_validation_${validationStatus}`]: validationStatus,
+    Input_icon: icon,
   });
 
-  return <input className={className} {...rest} />;
+  return (
+    <div className={styles['Root']}>
+      <div className={styles['IconContainer']}>
+        {icon && <Icon type={icon} />}
+      </div>
+      <input className={inputClassName} {...rest} />
+    </div>
+  );
 };
 
 export default Input;
