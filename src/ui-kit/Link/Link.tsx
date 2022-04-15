98e88adb9b4beefba8ed6824a20d9f92e48f38ec
Parmaklot <babar.dmitry@gmail.com>
2022-04-15 17:51:20 +0300

    feat(Link): Add “primary” and “danger” themes.
    Also improve the component's typing. The children type depends on theme
    and omitted from the props specific to the certain link type.

--- src/ui-kit/Link/Link.tsx
+++ src/ui-kit/Link/Link.tsx
@@ -1,24 +1,29 @@
-import { HTMLProps } from 'react';
+import { HTMLAttributes, ReactNode } from 'react';
 import {
   Link as RouterLink,
   LinkProps as RouterLinkProps,
   NavLink,
   NavLinkProps,
 } from 'react-router-dom';
+import Text from 'components/Text';
 import styles from './Link.module.scss';
 
-interface ISharedProps {
-  theme?: 'wrapper' | 'primary';
-}
+type SharedProps =
+  | { theme: 'wrapper'; children: ReactNode }
+  | { theme?: 'primary' | 'danger'; children: string };
 
-type TypeSpecificProps =
-  | ({ type: 'RouterLink' } & RouterLinkProps)
-  | ({ type: 'RouterNavLink' } & NavLinkProps)
-  | ({ type: 'HTMLAnchor' } & HTMLProps<HTMLAnchorElement>);
+type RouterLinkTypeProps = Omit<RouterLinkProps, 'children'>;
+type NavLinkTypeProps = Omit<NavLinkProps, 'children'>;
+type AnchorTypeProps = Omit<HTMLAttributes<HTMLAnchorElement>, 'children'>;
 
-type IProps = ISharedProps & TypeSpecificProps;
+export type LinkTypeSpecificProps =
+  | ({ type: 'RouterLink' } & RouterLinkTypeProps)
+  | ({ type: 'RouterNavLink' } & NavLinkTypeProps)
+  | ({ type: 'HTMLAnchor' } & AnchorTypeProps);
 
-const Link: React.FC<IProps> = ({
+type IProps = SharedProps & LinkTypeSpecificProps;
+
+const Link: React.VFC<IProps> = ({
   theme = 'primary',
   type,
   children,
@@ -26,25 +31,32 @@ const Link: React.FC<IProps> = ({
 }) => {
   const className = styles[`Root_theme_${theme}`];
 
+  const content =
+    theme === 'wrapper' ? (
+      children
+    ) : (
+      <Text color="inherit">{children as string}</Text>
+    );
+
   switch (type) {
     case 'RouterLink':
       return (
-        <RouterLink className={className} {...(rest as RouterLinkProps)}>
-          {children}
+        <RouterLink className={className} {...(rest as RouterLinkTypeProps)}>
+          {content}
         </RouterLink>
       );
 
     case 'RouterNavLink':
       return (
-        <NavLink className={className} {...(rest as NavLinkProps)}>
-          {children}
+        <NavLink className={className} {...(rest as NavLinkTypeProps)}>
+          {content}
         </NavLink>
       );
 
     case 'HTMLAnchor':
       return (
-        <a className={className} {...(rest as HTMLProps<HTMLAnchorElement>)}>
-          {children}
+        <a className={className} {...(rest as AnchorTypeProps)}>
+          {content}
         </a>
       );
   }
