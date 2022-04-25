1c2af785d73d293f5a38544d139b0ea23c3f8c8a
Parmaklot <babar.dmitry@gmail.com>
2022-04-25 16:06:30 +0300

    feat(Form): Adjust the Column subcomponent

--- src/ui-kit/Form/components/Column/Column.tsx
+++ src/ui-kit/Form/components/Column/Column.tsx
@@ -1,53 +1,80 @@
 import classNames from 'classnames/bind';
 import { Children, cloneElement, ComponentProps, ReactElement } from 'react';
 import Button from 'ui-kit/Button';
+import CustomItem from '../CustomItem';
 import Item from '../Item';
+import Row from '../Row';
 import styles from './Column.module.scss';
+
+type RowComponent = typeof Row;
+type RowElement = ReactElement<ComponentProps<RowComponent>>;
 
 type ItemComponent = typeof Item;
 type ItemElement = ReactElement<ComponentProps<ItemComponent>>;
 
+type CustomItemComponent = typeof CustomItem;
+type CustomItemElement = ReactElement<ComponentProps<CustomItemComponent>>;
+
 type ButtonComponent = typeof Button;
 type ButtonElement = ReactElement<ComponentProps<ButtonComponent>>;
 
-type Child = ItemElement | ButtonElement;
+type Child =
+  | RowElement
+  | RowElement[]
+  | ItemElement
+  | ItemElement[]
+  | CustomItemElement
+  | CustomItemElement[]
+  | ButtonElement
+  | ButtonElement[];
 
 interface IProps {
-  _id?: number;
   _formLayout?: 'vertical' | 'horizontal';
   children: Child | Child[];
 }
 
 const cn = classNames.bind(styles);
 
-const Column: React.VFC<IProps> = ({ _id, _formLayout, children }) => {
+const Column: React.VFC<IProps> = ({ _formLayout, children }) => {
   return (
     <div className={styles['Root']}>
-      {Children.map(children, (child, index) => {
-        switch (child.type) {
-          default:
-            throw new Error(
-              'Only <Form.Item> or <Button> can be used as a child of <Form.Column>'
-            );
+      {Children.map(children, (childNode) => {
+        const elements = Array.isArray(childNode) ? childNode : [childNode];
 
-          case Item:
-            return cloneElement(child as ItemElement, {
-              _id: `column-${_id}-item-${index}`,
-              _formLayout,
-            });
+        const validated = elements.map((element) => {
+          switch (element.type) {
+            default:
+              throw new Error(
+                'Only <Form.Row>, <Form.Item>, <Form.CustomItem> or <Button> can be used as a child of <Form.Column>'
+              );
 
-          case Button:
-            return (
-              <div
-                className={cn(
-                  'ButtonContainer',
-                  `ButtonContainer_layout_${_formLayout}`
-                )}
-              >
-                {cloneElement(child as ButtonElement)}
-              </div>
-            );
-        }
+            case Row:
+              return cloneElement(element as RowElement, { _formLayout });
+
+            case Item:
+              return cloneElement(element as ItemElement, {
+                _formLayout,
+                _placement: 'inColumn',
+              });
+
+            case CustomItem:
+              return cloneElement(element as CustomItemElement);
+
+            case Button:
+              return (
+                <div
+                  className={cn(
+                    'ButtonContainer',
+                    `ButtonContainer_layout_${_formLayout}`
+                  )}
+                >
+                  {cloneElement(element as ButtonElement)}
+                </div>
+              );
+          }
+        });
+
+        return validated;
       })}
     </div>
   );
