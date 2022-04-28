4b77945c4dd9d3f4ee2d96beb8184149a2ea72fa
Parmaklot <babar.dmitry@gmail.com>
2022-04-28 17:24:49 +0300

    feat: Add an InfoDisplay component

---
+++ src/ui-kit/InfoDisplay/InfoDisplay.tsx
@@ -0,0 +1,33 @@
+import classNames from 'classnames/bind';
+import { ComponentProps } from 'react';
+import Text from 'components/Text';
+import styles from './InfoDisplay.module.scss';
+
+interface IProps extends Pick<ComponentProps<typeof Text>, 'clamp'> {
+  theme?: 'neutral' | 'danger';
+  height?: 'auto' | 'fill';
+  children: string | string[];
+}
+
+const cn = classNames.bind(styles);
+
+const InfoDisplay: React.FC<IProps> = ({
+  theme = 'neutral',
+  height = 'auto',
+  clamp,
+  children,
+}) => {
+  const paragraphs = Array.isArray(children) ? children : [children];
+
+  return (
+    <div className={cn('Root', `Root_theme_${theme}`, `Root_height_${height}`)}>
+      {paragraphs.map((paragraph, i) => (
+        <p className={styles['Paragraph']} key={i}>
+          <Text {...clamp}>{paragraph}</Text>
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default InfoDisplay;
