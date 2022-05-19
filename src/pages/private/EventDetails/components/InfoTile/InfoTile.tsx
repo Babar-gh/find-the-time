2c057c77d1e52f625f3a696314a9e890cf58f2ac
Parmaklot <babar.dmitry@gmail.com>
2022-05-19 12:32:46 +0300

    feat(EventDetails): Add an InfoTile subcomponent

---
+++ src/pages/private/EventDetails/components/InfoTile/InfoTile.tsx
@@ -0,0 +1,27 @@
+import { ComponentProps } from 'react';
+import Icon from 'components/Icon';
+import Text from 'components/Text';
+import styles from './InfoTile.module.scss';
+
+interface IProps {
+  icon: ComponentProps<typeof Icon>['type'];
+  heading: string;
+}
+
+const InfoTile: React.FC<IProps> = ({ icon, heading, children }) => {
+  return (
+    <div className={styles['Root']}>
+      <div className={styles['Header']}>
+        <div className={styles['HeaderIcon']}>
+          <Icon type={icon} />
+        </div>
+        <Text font="primaryBold" clamp={1}>
+          {heading}
+        </Text>
+      </div>
+      <div className={styles['Content']}>{children}</div>
+    </div>
+  );
+};
+
+export default InfoTile;
