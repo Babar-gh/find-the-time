fd77bf29989b03b4484c21a1e877cec8dbb508e2
Parmaklot <babar.dmitry@gmail.com>
2022-05-20 00:01:28 +0300

    feat(EventDetails): Add isLoading state

--- src/pages/private/EventDetails/EventDetails.tsx
+++ src/pages/private/EventDetails/EventDetails.tsx
@@ -23,12 +23,15 @@ const EventDetails: React.VFC<IProps> = ({ navigateBackTo }) => {
   const { eventId } = useParams<PARAM.EventId>();
 
   const [details, setDetails] = useState<IEvent>();
+  const [isLoading, setIsLoading] = useState<boolean>();
 
   const account = useAppSelector((store) => store.account);
   const isOrganizer = details?.organizedBy.id === account.id;
 
   useEffect(() => {
     const fetchDetails = async () => {
+      setIsLoading(true);
+
       try {
         const response = await getEventDetails(eventId as Guid);
         const parsed = convertToIEvent(response.data);
@@ -37,35 +40,37 @@ const EventDetails: React.VFC<IProps> = ({ navigateBackTo }) => {
       } catch {
         // TODO: Replace with a proper error handling
       }
+
+      setIsLoading(false);
     };
 
     fetchDetails();
   }, [eventId]);
 
-  if (details) {
-    return (
-      <Page title={details.title} navigateBackTo={navigateBackTo}>
-        <div className={styles['Tiles']}>
-          <OrganizedBy
-            organizedBy={details.organizedBy}
-            isOrganizer={isOrganizer}
-          />
-          <Location location={details.location} />
-          <Duration duration={details.duration} />
-          <Status
-            chosenInterval={details.chosenInterval}
-            isOrganizer={isOrganizer}
-          />
-          <Comment comment={details.comment} />
-        </div>
-        <div className={styles['Subscriptions']}>
-          <Subscriptions subscriptions={details.subscriptions} />
-        </div>
-      </Page>
-    );
-  } else {
-    return <Page title="" isLoading />;
-  }
+  return (
+    <Page title={details?.title || ''} {...{ isLoading, navigateBackTo }}>
+      {details && (
+        <>
+          <div className={styles['Tiles']}>
+            <OrganizedBy
+              organizedBy={details.organizedBy}
+              isOrganizer={isOrganizer}
+            />
+            <Location location={details.location} />
+            <Duration duration={details.duration} />
+            <Status
+              chosenInterval={details.chosenInterval}
+              isOrganizer={isOrganizer}
+            />
+            <Comment comment={details.comment} />
+          </div>
+          <div className={styles['Subscriptions']}>
+            <Subscriptions subscriptions={details.subscriptions} />
+          </div>
+        </>
+      )}
+    </Page>
+  );
 };
 
 export default EventDetails;
