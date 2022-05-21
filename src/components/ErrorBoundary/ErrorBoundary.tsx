82d0e6081754bf1b23346c8b5731f8f0e31bab9c
Parmaklot <babar.dmitry@gmail.com>
2022-05-22 02:35:07 +0300

    refactor(ErrorBoundary): Simplify class declaration

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -11,13 +11,13 @@ interface IState {
 }
 
 class ErrorBoundary extends Component<IProps, IState> {
-  public state: IState = { hasError: false };
+  state: IState = { hasError: false };
 
-  public static getDerivedStateFromError(_: Error): IState {
+  static getDerivedStateFromError(_: Error): IState {
     return { hasError: true };
   }
 
-  public render() {
+  render() {
     return this.state.hasError ? (
       <Navigate to={PRIVATE.Error} />
     ) : (
