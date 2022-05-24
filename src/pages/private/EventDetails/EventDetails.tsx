973ea1846290ecf2650101075d69e74ce14c8708
Parmaklot <babar.dmitry@gmail.com>
2022-05-24 12:50:26 +0300

    feat(EventDetails): Add user interactions
    The Unsubscribe button triggers unsubscribeFromEvent request.
    Shown to subscribers when event is notYetScheduled.
    
    The Delete button triggers removeEvent request.
    Shown to organizer when event is either pending or notYetScheduled.
    
    The Subscriptions component have an IconButton next to each subscriber
    which triggers removeUserFromEvent request.
    Shown to organizer when event is either pending or notYetScheduled.

--- src/pages/private/EventDetails/EventDetails.tsx
+++ src/pages/private/EventDetails/EventDetails.tsx
@@ -1,20 +1,30 @@
 import { To, useNavigate, useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import Button from 'ui-kit/Button';
 import Page from 'ui-kit/Page';
+import Scroll from 'ui-kit/Scroll';
 import Subscriptions from 'components/Subscriptions';
-import { getEventDetails } from 'api/events';
+import { getStatus, getUserRole } from 'helpers/events';
 import { Guid } from 'types/common';
 import { IEvent } from 'types/events';
+import { IUser } from 'types/users';
 import { PARAM, PRIVATE } from 'constants/routes';
 import { useAppSelector } from 'store/hooks';
+import {
+  getEventDetails,
+  removeEvent,
+  removeUserFromEvent,
+  unsubscribeFromEvent,
+} from 'api/events';
 import { LocationState } from '../NotFound';
-import Comment from './components/Comment';
-import Duration from './components/Duration';
-import Location from './components/Location';
-import OrganizedBy from './components/OrganizedBy';
-import Status from './components/Status';
-import styles from './EventDetails.module.scss';
 import { convertToIEvent } from './helpers';
+import styles from './EventDetails.module.scss';
+import Status from './components/Status';
+import OrganizedBy from './components/OrganizedBy';
+import Location from './components/Location';
+import InfoTile from './components/InfoTile';
+import Duration from './components/Duration';
+import Comment from './components/Comment';
 
 interface IProps {
   navigateBackTo: To;
@@ -36,48 +46,95 @@ const EventDetails: React.VFC<IProps> = ({ navigateBackTo }) => {
   const [isLoading, setIsLoading] = useState<boolean>();
 
   const account = useAppSelector((store) => store.account);
-  const isOrganizer = details?.organizedBy.id === account.id;
+
+  const role = details ? getUserRole(details, account) : null;
+  const status = details ? getStatus(details?.chosenInterval) : null;
+
+  const fetchDetails = useCallback(async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await getEventDetails(eventId as Guid);
+      const parsed = convertToIEvent(response.data);
+
+      setDetails(parsed);
+
+      setIsLoading(false);
+    } catch {
+      setIsLoading(false);
+
+      navigate(PRIVATE.NotFound, { state: notFoundPageConfig });
+    }
+  }, [eventId, navigate]);
 
   useEffect(() => {
-    const fetchDetails = async () => {
-      setIsLoading(true);
+    fetchDetails();
+  }, [fetchDetails]);
 
-      try {
-        const response = await getEventDetails(eventId as Guid);
-        const parsed = convertToIEvent(response.data);
+  const handleDeleteButtonClick = async () => {
+    await removeEvent(eventId as Guid);
 
-        setDetails(parsed);
+    navigate(PRIVATE.Events);
+  };
 
-        setIsLoading(false);
-      } catch {
-        setIsLoading(false);
-
-        navigate(PRIVATE.NotFound, { state: notFoundPageConfig });
-      }
-    };
+  const handleUserRemoval = async (user: IUser) => {
+    await removeUserFromEvent(eventId as Guid, user.id);
 
     fetchDetails();
-  }, [eventId, navigate]);
+  };
+
+  const handleUnsubscribeButtonClick = async () => {
+    await unsubscribeFromEvent(eventId as Guid);
+
+    fetchDetails();
+  };
 
   return (
     <Page title={details?.title || ''} {...{ isLoading, navigateBackTo }}>
-      {details && (
+      {details && role && status && (
         <>
           <div className={styles['Tiles']}>
-            <OrganizedBy
-              organizedBy={details.organizedBy}
-              isOrganizer={isOrganizer}
-            />
+            <OrganizedBy organizedBy={details.organizedBy} role={role} />
             <Location location={details.location} />
             <Duration duration={details.duration} />
             <Status
               chosenInterval={details.chosenInterval}
-              isOrganizer={isOrganizer}
+              role={role}
+              status={status}
             />
             <Comment comment={details.comment} />
           </div>
           <div className={styles['Subscriptions']}>
-            <Subscriptions subscriptions={details.subscriptions} />
+            <InfoTile icon="People" heading="Participants">
+              <Scroll axis="horizontal">
+                <Subscriptions
+                  subscriptions={details.subscriptions}
+                  onUserRemoval={
+                    role === 'organizer' && status !== 'past'
+                      ? handleUserRemoval
+                      : undefined
+                  }
+                />
+              </Scroll>
+            </InfoTile>
+          </div>
+          <div>
+            {role === 'subscriber' && status === 'notYetScheduled' && (
+              <Button
+                theme="danger"
+                elementProps={{ onClick: handleUnsubscribeButtonClick }}
+              >
+                Unsubscribe
+              </Button>
+            )}
+            {role === 'organizer' && status !== 'past' && (
+              <Button
+                theme="danger"
+                elementProps={{ onClick: handleDeleteButtonClick }}
+              >
+                Delete
+              </Button>
+            )}
           </div>
         </>
       )}
