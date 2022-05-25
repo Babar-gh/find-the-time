348807f3f098b4c83904265743fd7a7efb834ad2
Parmaklot <babar.dmitry@gmail.com>
2022-05-25 04:11:21 +0300

    feat(DatePicker): Add constraints prop

--- src/ui-kit/DatePicker/DatePicker.tsx
+++ src/ui-kit/DatePicker/DatePicker.tsx
@@ -1,15 +1,22 @@
 import { DateTimePicker } from '@mui/x-date-pickers';
 import { Dayjs } from 'dayjs';
 import { useState } from 'react';
+import { TimeInterval } from 'types/common';
 import Input from 'ui-kit/Input';
 
 interface IProps {
   id?: string;
   value: Dayjs | null;
   onChange: (date: Dayjs | null) => void;
+  constraints?: TimeInterval;
 }
 
-const DatePicker: React.VFC<IProps> = ({ id, value, onChange }) => {
+const DatePicker: React.VFC<IProps> = ({
+  id,
+  value,
+  onChange,
+  constraints,
+}) => {
   const [open, setOpen] = useState(false);
 
   return (
@@ -19,6 +26,8 @@ const DatePicker: React.VFC<IProps> = ({ id, value, onChange }) => {
       open={open}
       onOpen={() => setOpen(true)}
       onClose={() => setOpen(false)}
+      minDateTime={constraints?.start}
+      maxDateTime={constraints?.end}
       renderInput={({ inputRef, inputProps }) => {
         return (
           <Input
