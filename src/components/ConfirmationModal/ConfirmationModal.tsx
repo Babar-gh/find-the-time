2602c5f2b247a3bb7ad02199fc2352a68591e737
Parmaklot <babar.dmitry@gmail.com>
2022-06-07 18:58:08 +0300

    feat: Add a ConfirmationModal component

---
+++ src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -0,0 +1,28 @@
+import Text from 'components/Text';
+import Modal from 'ui-kit/Modal';
+
+interface IProps {
+  title: string;
+  action: string;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+const ConfirmationModal: React.VFC<IProps> = ({
+  title,
+  action,
+  onConfirm,
+  onCancel,
+}) => (
+  <Modal
+    title={title}
+    onOkClick={onConfirm}
+    onCancelClick={onCancel}
+    onCloseClick={onCancel}
+    onBackdropClick={onCancel}
+  >
+    <Text>You are about to {action}. Do you want to proceed?</Text>
+  </Modal>
+);
+
+export default ConfirmationModal;
