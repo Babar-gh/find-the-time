ba742553ae91ebce6ee3e5a9a7f3304f951db727
Parmaklot <babar.dmitry@gmail.com>
2022-06-20 03:10:34 +0300

    feat(demo): Change schedule templates structure

--- src/constants/formats.ts
+++ src/constants/formats.ts
@@ -1,6 +1,7 @@
+export const DATE_DEFAULT = 'YYYY-MM-DD';
 export const DATE_SHORT = 'MMM D';
 
 export const TIME_DEFAULT = 'HH:mm';
 
-export const DATETIME_DEFAULT = 'YYYY-MM-DD HH:mm:ss';
+export const DATETIME_DEFAULT = `${DATE_DEFAULT} HH:mm:ss`;
 export const DATETIME_PICKER = `DD/MM/YYYY ${TIME_DEFAULT}`;
--- src/demo/templates/schedules.ts
+++ src/demo/templates/schedules.ts
@@ -1,25 +1,77 @@
-export const schedules = [
-  {
-    duration: 123,
+import dayjs from 'dayjs';
+import { DATE_DEFAULT } from 'constants/formats';
+
+const now = dayjs();
+
+const templates = [
+  ([first, second]: string[]) => ({
+    duration: 45,
     subscriptions: [
       {
         availability: [
-          { start: '2022-03-13 12:00:00', end: '2022-03-14 12:00:00' },
-          { start: '2022-03-11 12:00:00', end: '2022-03-12 12:00:00' },
+          {
+            start: `${first} 08:00:00`,
+            end: `${first} 18:00:00`,
+          },
+          {
+            start: `${second} 09:00:00`,
+            end: `${second} 20:00:00`,
+          },
         ],
       },
       {
         availability: [
-          { start: '2022-03-12 03:14:00', end: '2022-03-12 12:00:00' },
-          { start: '2022-03-11 17:20:00', end: '2022-03-11 23:10:00' },
-          { start: '2022-03-13 22:56:00', end: '2022-03-14 12:00:00' },
+          {
+            start: `${first} 10:00:00`,
+            end: `${first} 16:00:00`,
+          },
+          {
+            start: `${second} 09:00:00`,
+            end: `${second} 14:00:00`,
+          },
+          {
+            start: `${second} 17:00:00`,
+            end: `${second} 20:00:00`,
+          },
+        ],
+      },
+      {
+        availability: [
+          {
+            start: `${first} 08:00:00`,
+            end: `${first} 14:00:00`,
+          },
+          {
+            start: `${second} 11:00:00`,
+            end: `${second} 18:00:00`,
+          },
+        ],
+      },
+      {
+        availability: [
+          {
+            start: `${first} 12:00:00`,
+            end: `${first} 18:00:00`,
+          },
+          {
+            start: `${second} 13:00:00`,
+            end: `${second} 19:00:00`,
+          },
         ],
       },
     ],
     chosenInterval: {
-      start: '2022-03-11 17:20:00',
-      end: '2022-03-11 19:23:00',
+      start: `${first} 13:15:00`,
+      end: `${first} 14:00:00`,
     },
-    created: '2022-03-14 14:08:30',
-  },
+  }),
 ];
+
+const daySets = [
+  [now.add(1, 'day'), now.add(2, 'day'), now.add(3, 'day')],
+  [now.subtract(3, 'day'), now.subtract(2, 'day'), now.subtract(1, 'day')],
+].map((sets) => sets.map((day) => day.format(DATE_DEFAULT)));
+
+export const schedules = daySets.flatMap((set) =>
+  templates.map((fillTemplate) => fillTemplate(set))
+);
